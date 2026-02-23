905a3eb21ac01cb6012a69228b5dba1de32a2f10
Edd3452 <edd9934@gmail.com>
2026-02-23 14:14:21 -0600

    Actualización

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,7 +49,7 @@ with st.sidebar:
 
     st.header("Menús Disponibles")
     st.info("Seleccione las capas desde el menú desplegable.")
-    st.sidebar.write("Variables 2026")
+    st.sidebar.write("v2.3 - Submenus generated")
 
 # Custom CSS for white background
 st.markdown('''
@@ -155,21 +155,23 @@ if os.path.exists(shapefiles_dir):
             "Utopías": "Social/utopias.shp",
             "Centros de Justicia": "Social/Centros_de_justicia.shp"
         },
-        "Delitos": {
+        "Delitos a personas": {
             "Homicidios": "Delitos/Homicidios.shp",
-            "Robo a Casa Habitación": "Delitos/ROBO A CASA HABITACIÓN .shp",
-            "Robo a Cuentahabiente": "Delitos/ROBO A CUENTAHABIENTE.shp",
-            "Robo a Negocio con Violencia": "Delitos/ROBO A NEGOCIO CON VIOLENCIA.shp",
+            "Violaciones": "Delitos/VIOLACIONES.shp",
             "Robo a Pasajero en Microbús": "Delitos/ROBO A PASAJERO A BORDO DE MICROBUS.shp",
             "Robo a Pasajero en Taxi": "Delitos/ROBO A PASAJERO A BORDO DE TAXI.shp",
             "Robo a Pasajero en Metro": "Delitos/ROBO A PASAJERO A BORDO DEL METRO.shp",
+            "Robo a Cuentahabiente": "Delitos/ROBO A CUENTAHABIENTE.shp",
+            "Robo a Casa Habitación": "Delitos/ROBO A CASA HABITACIÓN .shp",
+            "Robo a Negocio con Violencia": "Delitos/ROBO A NEGOCIO CON VIOLENCIA.shp"
+        },
+        "Delitos con vehículos implicados": {
             "Robo a Repartidor": "Delitos/ROBO A REPARTIDOR.shp",
             "Robo a Transportista": "Delitos/ROBO A TRASPORTISTA.shp",
             "Robo de Motocicleta con Violencia": "Delitos/ROBO DE MOTOCICLETA CON VIOLENCIA.shp",
             "Robo de Vehículo Particular con Violencia": "Delitos/ROBO DE VEHICULO DE SERVICIO PARTICULAR CON VIOLENCIA.shp",
             "Robo de Vehículo Público sin Violencia": "Delitos/ROBO DE VEHICULO DE SERVICIO PÚBLICO SIN VIOLENCIA.shp",
-            "Robo de Vehículo": "Delitos/ROBO DE VEHICULO.shp",
-            "Violaciones": "Delitos/VIOLACIONES.shp"
+            "Robo de Vehículo": "Delitos/ROBO DE VEHICULO.shp"
         },
         "Socio-Demográfico": {
             "Índice de Desarrollo": "Socio demografico/alcd.shp",
